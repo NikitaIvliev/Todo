d4ff05438c78cb06364bb23d7b5c38a25022eef0
NikitaIvliev <Nikitka.ivliev2013@mail.ru>
2023-10-12 23:54:42 +0300

    22

---
+++ Todo.Entities/UserModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Todo.Entities
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+
+}
